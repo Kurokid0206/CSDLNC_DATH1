1db4b356d0ecc9ef7890c5de028b4e80280d191a
QuocBinh115 <93441479+QuocBinh115@users.noreply.github.com>
2021-11-05 22:04:35 +0700

    Điều khiển hiển thị form

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,1 +1,37 @@
+//Show the bill form and turn off all others
+function show_bill_form() {
+    var div = document.querySelector("#form-insert-bill")
+    div.style.display = "block";
+    div = document.querySelector("#form-insert-details")
+    div.style.display = "none";
+    div = document.querySelector("#bills-container")
+    div.style.display = "none";
+    div = document.querySelector("#select-sales-type")
+    div.style.display = "none";
+}
+//Show the details form and turn off all others
+function show_details_form() {
+    var div = document.querySelector("#form-insert-bill")
+    div.style.display = "none";
+    div = document.querySelector("#form-insert-details")
+    div.style.display = "block";
+    div = document.querySelector("#bills-container")
+    div.style.display = "none";
+    div = document.querySelector("#select-sales-type")
+    div.style.display = "none";
+}
+
+//Show the sales table and turn off all others
+function show_sales() {
+    var div = document.querySelector("#form-insert-bill")
+    div.style.display = "none";
+    div = document.querySelector("#form-insert-details")
+    div.style.display = "none";
+    div = document.querySelector("#bills-container")
+    div.style.display = "none";
+    div = document.querySelector("#select-sales-type")
+    div.style.display = "block";
+
+}
+
 
